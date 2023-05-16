4996a3e38d4645e56e87262290de7ad204ab97fb
Jim Donovan <jimdon326@hotmail.co.uk>
2023-05-16 20:36:17 +0100

    Nest pokemon into single view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,12 @@ import { RegionListComponent } from './region/region-list/region-list.component'
 
 const routes: Routes = [
   { path: '', redirectTo: 'regions', pathMatch: 'full' },
-  { path: 'regions', component: RegionListComponent },
-  { path: 'region-pokemon/:id', component: RegionPokemonComponent },
-  { path: 'pokemon/:id', component: PokemonComponent },
+  { path: 'regions', component: RegionListComponent,
+    children: [
+      { path: 'region-pokemon/:id', component: RegionPokemonComponent },
+      { path: 'region-pokemon/:id/pokemon/:id', component: PokemonComponent }
+    ] 
+  },
 ];
 
 @NgModule({
